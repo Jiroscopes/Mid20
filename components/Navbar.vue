60c16b737598155d5459c68878cbd7e48306aed6
Steven <steven.popick@gmail.com>
2020-11-16 16:54:21 -0600

    Changed Favicon and Site title

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="bg-white flex-wrap justify-between items-center px-6 py-3 w-full fixed z-20">
+    <nav class="bg-white flex-wrap justify-between items-center px-6 py-3 w-full fixed z-30">
         <div class="block lg:hidden">
             <button @click="toggleNav" class="flex items-center px-3 py-2 border rounded text-blue border-darkBlue hover:text-darkBlue hover:border-darkBlue">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
