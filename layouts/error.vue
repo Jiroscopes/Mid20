28f87c3bc5a84c865ed2ea5de7471f4e2e857519
Steven Popick <steven.popick@gmail.com>
2020-12-14 17:56:08 -0600

    added 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="py-20">
+    <h1 v-if="error.statusCode === 404" class="text-center block font-OpenSans text-6xl text-blue font-bold">Whoa! Wrong turn.</h1>
+    <h1 v-else class="text-center block font-OpenSans text-6xl text-blue font-bold">An error occurred</h1>
+    <div class="text-center">
+        <NuxtLink class="text-DarkBlue underline font-OpenSans text-lg" to="/">Home page</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
