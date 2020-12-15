977e874f13da5c1a8e32e1871c793d6628503b9f
Steven <steven.popick@gmail.com>
2020-12-14 23:07:09 -0600

    Adding more to the project pages. Still working on the techbubbles

--- components/LargePortfolio.vue
+++ components/LargePortfolio.vue
@@ -7,7 +7,7 @@
         <!-- Right Side -->
         <div class="col-span-full md:col-span-2 bg-cover md:bg-none flex items-center">
             <div class="px-16">
-                <h3 class="font-OpenSans font-bold text-orange text-4xl block mt-8 md:mt-16">{{title}}</h3>
+                <h3 class="font-OpenSans font-bold text-orange text-2xl block mt-8 md:mt-16">{{title}}</h3>
                 <p class="font-Nunito font-bold text-blue text-xl block mt-4">{{type}}</p>
                 <NuxtLink class="font-Nunito font-semibold text-darkBlue underline text-md my-8 md:mb-0 md:mt-16 block" :to="link">Read More</NuxtLink>
             </div>
--- pages/work/_portfolio.vue
+++ pages/work/_portfolio.vue
@@ -1,9 +1,30 @@
 <template>
-    <div id="head">
+    <div id="head" class="bg-white ">
         <Navbar />
-        <div class="bg-white">
-            <div class="px-32 py-20 ">
-                <img class="shadow-xl" :src="'/' + getPiece() + '/full.png'" :alt="getPiece() + ' Image'">
+        <div class="md:px-32 px-2">
+            <div class="py-32 text-center md:text-left">
+                <h1 class="text-blue font-bold font-OpenSans md:text-6xl text-3xl">
+                    {{capitializeName()}}
+                </h1>
+                <div class="grid grid-flow-row xl:grid-cols-7 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-8 gap-0" >
+                    <div class="col-span-1 my-4 md:px-2">
+                        <TechBubble text="JavaScript" />
+                    </div>
+                    <div class="col-span-1 my-4 md:px-2">
+                        <TechBubble text="PHP" />
+                    </div> 
+                    <div class="col-span-1 my-4 md:px-2">
+                        <TechBubble text="WordPress" />
+                    </div>  
+                    <div class="col-span-1 my-4 md:px-2">
+                        <TechBubble text="HTML/CSS" />
+                    </div> 
+                </div>
+            </div>
+            <div class="bg-white">
+                <div class=" py-10 ">
+                    <img class="shadow-xl" :src="'/' + getPiece() + '/full.png'" :alt="getPiece() + ' Image'">
+                </div>
             </div>
         </div>
     </div>
@@ -17,6 +38,12 @@ import Component from 'vue-class-component';
 export default class index extends Vue {
     data() {
         return {};
+    }
+
+    capitializeName(): string {
+        let name = this.getPiece();
+        let nameCap = name.charAt(0).toUpperCase() + name.slice(1);
+        return nameCap;
     }
 
     getPiece(): string {
